666e81bad00f6df1ea0d6323d6140fef2711b7a2
tevredensmartplantgebruiker <55087236+tevredensmartplantgebruiker@users.noreply.github.com>
2019-11-14 14:28:23 +0100

    contactinformatie en contactgegevens

--- Pages/contact_page.php
+++ Pages/contact_page.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link type="text/css" rel="stylesheet" href="/css/bootstrap.css">
+    <link type="text/css" rel="stylesheet" href="/css/imports.css">
 </head>
 <body>
 <h1>Contact</h1>
