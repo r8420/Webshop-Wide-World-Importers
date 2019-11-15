f4d3fdf7b4ff0cf680702ec73688937e5ff3f53b
tevredensmartplantgebruiker <55087236+tevredensmartplantgebruiker@users.noreply.github.com>
2019-11-15 10:33:01 +0100

    Update contact_page.php

--- Pages/contact_page.php
+++ Pages/contact_page.php
@@ -7,19 +7,26 @@
     <link type="text/css" rel="stylesheet" href="../css/imports.css">
 </head>
 <body>
-<h1>Contact</h1>
-<h2>Contact opnemen</h2>
-<p>Heeft u vragen over één van de producten die wij verkopen of heeft u een probleem met uw bestelling? Om een antwoord
-    op deze en andere vragen te krijgen kunt u ons op maandag t/m vrijdag bereiken via onze <em class="text-info">telefonische
-        klantenservice.</em>
-</p>
-<h2>Contactgegevens</h2>
-<div class="container"><p>WWI
-        Torenstraat 118
-        Dordrecht
-        Zuid-Holland
-        3311 TP
-        +31 9404393940
-        wwi@nfiewfoewnf.nl</p></div>
+<div class="container">
+    <div>
+        <div class="mt-3">
+            <h1>Contact</h1>
+        </div>
+        <h2>Contact opnemen</h2>
+        <p>Heeft u vragen over één van de producten die wij verkopen of heeft u een probleem met uw bestelling? Om een
+            antwoord
+            op deze en andere vragen te krijgen kunt u ons op maandag t/m vrijdag bereiken via onze <em class="text-info">telefonische
+                klantenservice.</em>
+        </p>
+        <h2>Contactgegevens</h2>
+        <div class="container"><b><p>
+                    WWI<br>
+                    Torenstraat 118<br>
+                    Dordrecht<br>
+                    Zuid-Holland<br>
+                    3311 TP<br>
+                    +31 9404393940<br>
+                    wwi@nfiewfoewnf.nl</b></p></div>
+    </div>
 </body>
 </html>/
