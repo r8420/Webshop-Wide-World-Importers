a166eb0d02fdb2a932b863ff347178d82fa077dc
myousfi1 <myousfi703@gmail.com>
2019-11-19 21:03:48 +0100

    sdsd

--- Modules/footer.php
+++ Modules/footer.php
@@ -1,8 +1,8 @@
 <footer class="text-white sticky-bottom bg-primary mt-5">
     <div class="container">
-        <div class="row pt-3 pb-4 ">
-            <div class="col-6">
-                <img class="float-left mr-1" src="../Images/logo.png" width="40px" height="40px">
+        <div class="row">
+            <div class="col-sm-6 pt-3 pb-4">
+                <img class="float-left mr-1" src="Images/logo.png" width="40px" height="40px">
                 <h2>WWI</h2>
                 <p class="pt-2">Lorem sdsdsdsddsdipsum dolor sit amet, consectetur adipiscing elit. Morbi et nisl
                     hendrerit, aliquet mi sed, scelerisque tortor. Aliquam eu scelerisque quam, ac tristique dolor.
@@ -10,7 +10,7 @@
                     aliquet augue nec, molestie felis. Fusce porta.</p>
 
             </div>
-            <div class="col-6">
+            <div class="col-sm-6 pt-3 pb-4">
                 <h2>Klantenservice</h2>
                 <p class="pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et nisl hendrerit,
                     aliquet mi sed, scelerisque tortor. Aliquam eu scelerisque quam, ac tristique dolor. Aliquam nulla
