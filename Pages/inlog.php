ac2346f14922457da03abbde9196960722c18b80
sanderpol <sander__pol@outlook.com>
2019-11-19 12:17:53 +0000

    update inlog page

--- Pages/inlog.php
+++ Pages/inlog.php
@@ -1,63 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Inloggen</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../css/imports.css">
-    <style>
-        .container {
-            margin: 100px auto;
-            max-width: 400px;
-            text-align: center;
-        }
-        #inlogbutton {
-            width: 120px;
-            margin-top: 6px;
-        }
-        .mb-13px {
-            margin-bottom: 13px;
-        }
-        .input-group-text {
-            background-color: unset;
-            border: unset;
-        }
-        .input-group-text .fas {
-            font-size: 1.5rem;
-            padding-right: 15px;
-        }
-        #InputGebruikersnaam, #InputWachtwoord1 {
-            border-radius: .25rem;
-        }
-    </style>
-</head>
-<body>
-
 <?php
 include '../Modules/header.php'
 ?>
 <!-- Begin page content -->
-<div class="container">
-    <div class="mb-5">
+<div class="container col-sm-3 ">
+    <div class="m-5 text-center">
         <h1>Login</h1>
     </div>
     <form>
-        <div class="input-group mb-3">
+        <div class="input-group  mb-3">
             <div class="input-group-prepend">
                 <div class="input-group-text"><i class="fas fa-user"></i></div>
             </div>
-            <input type="text" class="form-control" id="InputGebruikersnaam" placeholder="Gebruikersnaam" required>
+            <input type="text" class="form-control rounded" placeholder="Gebruikersnaam" required>
         </div>
-        <div class="input-group mb-3">
+        <div class="input-group  mb-3">
             <div class="input-group-prepend">
                 <div class="input-group-text"><i class="fas fa-lock"></i></div>
             </div>
-            <input type="password" class="form-control" id="InputWachtwoord1" placeholder="Wachtwoord" required>
+            <input type="password" class="form-control rounded" placeholder="Wachtwoord" required>
         </div>
-        <button type="submit" id="inlogbutton" class="btn btn-success mb-13px">Inloggen</button>
+        <div class=" row justify-content-center">
+            <button type="submit" id="inlogbutton" class="btn btn-success pl-4 px-4 mb-3">Inloggen</button>
+        </div>
     </form>
-    <p>
+    <p class="text-center">
         Nog geen account?<br>
         <a href="registreren.php">Maak hier een account aan</a>
     </p>
@@ -66,5 +32,3 @@ include '../Modules/header.php'
 <?php
 include '../Modules/footer.php'
 ?>
-</body>
-</html>
