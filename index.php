d7b7d6079441a51d59cf7a8a6e9b3fc604b51771
myousfi1 <myousfi703@gmail.com>
2019-11-19 14:23:04 +0100

    homepagina

--- index.php
+++ index.php
@@ -2,21 +2,37 @@
 include "Modules/header.php";
 ?>
 
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
+<div class="container">
+<div id="carouselExampleIndicators" class="carousel slide mt-5 " data-ride="carousel">
+    <ol class="carousel-indicators rounded">
         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
     </ol>
-    <div class="carousel-inner">
+    <div class="carousel-inner rounded">
         <div class="carousel-item active">
-            <img class="d-block w-100" src=".../800x400?auto=yes&bg=777&fg=555&text=First slide" alt="First slide">
+            <img class="d-block w-100" src="https://socialbrothers.nl/wp-content/uploads/2016/11/r_atr-header-main.jpg" alt="First slide">
+            <div class="carousel-caption d-none d-md-block justify-content-start">
+                <h5>Productnaam 1</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et nisl hendrerit, aliquet mi sed, scelerisque tortor. Aliquam eu scelerisque quam, ac tristique dolor. Aliquam nulla risus, fermentum feugiat tortor quis, facilisis cursus arcu.</p>
+                <button type="button" class="btn btn-success text-white">Bekijken </button>
+            </div>
+        </div>
+        <div class="carousel-item rounded" >
+            <img class="d-block w-100" src="https://socialbrothers.nl/wp-content/uploads/2016/11/r_atr-header-main.jpg" alt="Second slide">
+            <div class="carousel-caption d-none d-md-block justify-content-start">
+                <h5>Productnaam 2</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et nisl hendrerit, aliquet mi sed, scelerisque tortor. Aliquam eu scelerisque quam, ac tristique dolor. Aliquam nulla risus, fermentum feugiat tortor quis, facilisis cursus arcu.</p>
+                <button type="button" class="btn btn-success text-white">Bekijken </button>
+            </div>
         </div>
         <div class="carousel-item">
-            <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide">
-        </div>
-        <div class="carousel-item">
-            <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide">
+            <img class="d-block w-100" src="https://socialbrothers.nl/wp-content/uploads/2016/11/r_atr-header-main.jpg" alt="Third slide">
+            <div class="carousel-caption d-none d-md-block justify-content-start">
+                <h5>Productnaam 3</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et nisl hendrerit, aliquet mi sed, scelerisque tortor. Aliquam eu scelerisque quam, ac tristique dolor. Aliquam nulla risus, fermentum feugiat tortor quis, facilisis cursus arcu.</p>
+                <button type="button" class="btn btn-success text-white">Bekijken </button>
+            </div>
         </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -27,6 +43,257 @@ include "Modules/header.php";
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
     </a>
+</div>
+
+    <div class="row mt-5">
+        <div class="col-3">
+            <a href="#" class="text-decoration-none">
+            <div class="card border-0">
+                <img src="Images/product1.png" class="card-img-top w-75 mx-auto pt-3" alt="...">
+                <div class="card-body">
+                    <h3 class="card-title h5 text-dark">Productnaam</h3>
+                    <i class="fas fa-star rating"></i>
+                    <i class="fas fa-star rating"></i>
+                    <i class="fas fa-star rating"></i>
+                    <i class="fas fa-star rating"></i>
+                    <i class="fas fa-star-half-alt rating"></i>
+                </div>
+            </div>
+            </a>
+        </div>
+        <div class="col-3">
+            <a href="#" class="text-decoration-none">
+                <div class="card border-0">
+                    <img src="Images/product2.png" class="card-img-top w-75 mx-auto pt-3" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title h5 text-dark">Productnaam</h3>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star-half-alt rating"></i>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-3">
+            <a href="#" class="text-decoration-none">
+                <div class="card border-0">
+                    <img src="Images/product3.png" class="card-img-top w-75 mx-auto pt-3" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title h5 text-dark">Productnaam</h3>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star-half-alt rating"></i>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-3">
+            <a href="#" class="text-decoration-none">
+                <div class="card border-0">
+                    <img src="Images/product4.png" class="card-img-top w-75 mx-auto pt-3" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title h5 text-dark">Productnaam</h3>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star-half-alt rating"></i>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-3">
+            <a href="#" class="text-decoration-none">
+                <div class="card border-0">
+                    <img src="Images/product5.png" class="card-img-top w-75 mx-auto pt-3" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title h5 text-dark">Productnaam</h3>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star-half-alt rating"></i>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-3">
+            <a href="#" class="text-decoration-none">
+                <div class="card border-0">
+                    <img src="Images/product12.png" class="card-img-top w-75 mx-auto pt-3" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title h5 text-dark">Productnaam</h3>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star-half-alt rating"></i>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-3">
+            <a href="#" class="text-decoration-none">
+                <div class="card border-0">
+                    <img src="Images/product7.png" class="card-img-top w-75 mx-auto pt-3" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title h5 text-dark">Productnaam</h3>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star-half-alt rating"></i>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-3">
+            <a href="#" class="text-decoration-none">
+                <div class="card border-0">
+                    <img src="Images/product8.png" class="card-img-top w-75 mx-auto pt-3" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title h5 text-dark">Productnaam</h3>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star-half-alt rating"></i>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-3">
+            <a href="#" class="text-decoration-none">
+                <div class="card border-0">
+                    <img src="Images/product9.png" class="card-img-top w-75 mx-auto pt-3" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title h5 text-dark">Productnaam</h3>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star-half-alt rating"></i>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-3">
+            <a href="#" class="text-decoration-none">
+                <div class="card border-0">
+                    <img src="Images/product10.png" class="card-img-top w-75 mx-auto pt-3" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title h5 text-dark">Productnaam</h3>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star-half-alt rating"></i>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-3">
+            <a href="#" class="text-decoration-none">
+                <div class="card border-0">
+                    <img src="Images/product11.png" class="card-img-top w-75 mx-auto pt-3" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title h5 text-dark">Productnaam</h3>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star-half-alt rating"></i>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-3">
+            <a href="#" class="text-decoration-none">
+                <div class="card border-0">
+                    <img src="Images/product12.png" class="card-img-top w-75 mx-auto pt-3" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title h5 text-dark">Productnaam</h3>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star rating"></i>
+                        <i class="fas fa-star-half-alt rating"></i>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-3">
+            <a href="#" class="text-decoration-none">
+                <div class="card align-items-center border-0">
+                    <div class="card-body">
+                        <h3 class="h4 text-dark">Categorie 1</h3>
+                    </div>
+                    <img src="Images/Categorie14.png" class="card-img-top w-75 mx-auto pt-3" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title h5 text-primary font-weight-bold">Bekijk categorie</h3>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-3">
+            <a href="#" class="text-decoration-none">
+                <div class="card align-items-center border-0">
+                    <div class="card-body">
+                        <h3 class="h4 text-dark">Categorie 2</h3>
+                    </div>
+                    <img src="Images/Categorie14.png" class="card-img-top w-75 mx-auto pt-3" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title h5 text-primary font-weight-bold">Bekijk categorie</h3>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-3">
+            <a href="#" class="text-decoration-none">
+                <div class="card align-items-center border-0">
+                    <div class="card-body">
+                        <h3 class="h4 text-dark">Categorie 3</h3>
+                    </div>
+                    <img src="Images/Categorie14.png" class="card-img-top w-75 mx-auto pt-3" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title h5 text-primary font-weight-bold">Bekijk categorie</h3>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-3">
+            <a href="#" class="text-decoration-none">
+                <div class="card align-items-center border-0">
+                    <div class="card-body">
+                        <h3 class="h4 text-dark">Categorie 4</h3>
+                    </div>
+                    <img src="Images/Categorie14.png" class="card-img-top w-75 mx-auto pt-3" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title h5 text-primary font-weight-bold">Bekijk categorie</h3>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+    </div>
+
+
 </div>
 
 
