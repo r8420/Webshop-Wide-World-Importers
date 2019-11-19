3136ca29a2eee3794af94b607676a9a44485022e
Richard Gonlag <rdgonlag@gmail.com>
2019-11-19 12:22:51 +0000

    Update Modules/footer.php, Modules/header.php files

--- Modules/footer.php
+++ Modules/footer.php
@@ -1,9 +1,9 @@
-<footer class="fixed-bottom bg-primary text-white">
+<footer class="text-white bg-primary mt-5">
     <div class="container">
         <div class="row pt-3 pb-4 ">
             <div class="col-6">
-                <img class="float-left pr-5" src="/Images/logo.png" width="40px" height="40px"><h2>WWI</h2>
-                <p class="pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et nisl hendrerit, aliquet mi sed, scelerisque tortor. Aliquam eu scelerisque quam, ac tristique dolor. Aliquam nulla risus, fermentum feugiat tortor quis, facilisis cursus arcu. Sed eu purus placerat, aliquet augue nec, molestie felis. Fusce porta.</p>
+                <img class="float-left mr-1" src="../Images/logo.png" width="40px" height="40px"><h2>WWI</h2>
+                <p class="pt-2">Lorem sdsdsdsddsdipsum dolor sit amet, consectetur adipiscing elit. Morbi et nisl hendrerit, aliquet mi sed, scelerisque tortor. Aliquam eu scelerisque quam, ac tristique dolor. Aliquam nulla risus, fermentum feugiat tortor quis, facilisis cursus arcu. Sed eu purus placerat, aliquet augue nec, molestie felis. Fusce porta.</p>
 
             </div>
             <div class="col-6">
