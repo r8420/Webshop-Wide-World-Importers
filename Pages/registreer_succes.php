fd87011c0e5457d22ee779c69679d648b737a67d
tevredensmartplantgebruiker <55087236+tevredensmartplantgebruiker@users.noreply.github.com>
2019-11-21 11:49:52 +0100

    Create registreer_succes.php

---
+++ Pages/registreer_succes.php
@@ -0,0 +1,33 @@
+<?php
+include '../Modules/head.php';
+include '../Modules/header.php';
+?>
+<!-- Begin page content -->
+<div class="container" align="center" style="margin-top: 100px; margin-bottom: 100px">
+    <div>
+        <img src="../Images/checkmark.png" class="mb-3" style="width: 199px; height: 188px>
+    </div>
+    <div class=" mb-3">
+        <h1>Registratie voltooid</h1>
+    </div>
+    <div class="container mb-4" align="center" style="width: 400px">
+        <p>
+           Het aanmaken van uw account is gelukt.
+        </p>
+        <p style="color: blue">
+
+        </p>
+    </div>
+    <a class="btn btn-success text-white" href="../index.php">Mijn account</a>
+    <br>
+    <br>
+    <a class="btn btn-success text-white" href="../index.php">Terug naar de homepagina</a>
+</div>
+
+<?php
+include '../Modules/footer.php'
+?>
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="../js/imports-dist.js"></script>
+</body>
+</html>
