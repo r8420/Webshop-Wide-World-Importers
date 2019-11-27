fe6cf111a908bcfe8fb673b99be3f782c46b098c
Vincent Houtjes <vincent@houtjes.com>
2019-11-27 10:53:24 +0100

    zoekfunctionaliteit - temp commit

--- Pages/zoekpagina.php
+++ Pages/zoekpagina.php
@@ -10,22 +10,55 @@ if($connection == false) {
 
 $search = ISSET($_GET['search']) ? $_GET['search'] : '';
 $category = ISSET($_GET['category']) ? $_GET['category'] : null;
+$orderBy = ISSET($_GET['order']) ? $_GET['order'] : 'nameAZ';
 
 $searchSQL = '%'.$search.'%';
 
+//PAGINATION CODE
+$page = ISSET($_GET['page']) ? $_GET['page'] : 1;
+$numOfItemsPerPage = 10;
+$offset = ($page - 1) * $numOfItemsPerPage;
+
+//Order by code
+$orderSQL = "ORDER BY StockItemName";
+switch ($orderBy) {
+    case 'nameAZ':
+        $orderSQL = "ORDER BY StockItemName";
+        break;
+    case 'nameZA':
+        $orderSQL = "ORDER BY StockItemName DESC";
+        break;
+    case 'priceHighLow':
+        $orderSQL = "ORDER BY RecommendedRetailPrice";
+        break;
+    case 'priceLowHigh':
+        $orderSQL = "ORDER BY RecommendedRetailPrice DESC";
+        break;
+}
+
 //Als de category niet gespecificeerd is
 if($category == null) {
-    $stmt = $connection->prepare("SELECT StockItemName, RecommendedRetailPrice, Photo FROM stockitems WHERE SearchDetails LIKE ?");
+    $stmt = $connection->prepare("SELECT StockItemName, RecommendedRetailPrice, Photo FROM stockitems WHERE SearchDetails LIKE ? $orderSQL LIMIT $offset, $numOfItemsPerPage");
     $stmt->bind_param("s", $searchSQL);
+
+    $paginationSTMT = $connection->prepare("SELECT COUNT(*) FROM stockitems WHERE SearchDetails LIKE ? ");
+    $paginationSTMT->bind_param("s", $searchSQL);
 } else {
-    $stmt = $connection->prepare("SELECT StockItemName, RecommendedRetailPrice, Photo, StockGroupName FROM stockitemstockgroups sisg JOIN stockitems si ON si.StockItemID = sisg.StockItemID JOIN stockgroups sg ON sg.StockGroupID = sisg.StockGroupID WHERE si.SearchDetails LIKE ? AND sisg.StockGroupID LIKE ?");
+    $stmt = $connection->prepare("SELECT StockItemName, RecommendedRetailPrice, Photo, StockGroupName FROM stockitemstockgroups sisg JOIN stockitems si ON si.StockItemID = sisg.StockItemID JOIN stockgroups sg ON sg.StockGroupID = sisg.StockGroupID WHERE si.SearchDetails LIKE ? AND sisg.StockGroupID LIKE ? $orderSQL LIMIT $offset, $numOfItemsPerPage");
     $stmt->bind_param("si", $searchSQL, $category);
+
+    $paginationSTMT = $connection->prepare("SELECT COUNT(*) FROM stockitemstockgroups sisg JOIN stockitems si ON si.StockItemID = sisg.StockItemID JOIN stockgroups sg ON sg.StockGroupID = sisg.StockGroupID WHERE si.SearchDetails LIKE ? AND sisg.StockGroupID LIKE ?");
+    $paginationSTMT->bind_param("si", $searchSQL, $category);
 }
 
 $stmt->execute();
 $result = $stmt->get_result();
 if($category != null)
     $categoryName = $result->fetch_assoc()['StockGroupName'];
+
+$paginationSTMT->execute();
+$numResults = $paginationSTMT->get_result()->fetch_array()[0];
+$totalPages = ceil($numResults / $numOfItemsPerPage);
 ?>
     <main class="container">
         <div class="row">
@@ -85,37 +118,59 @@ if($category != null)
                 <div class="card">
                     <div class="card-body">
                         <?php
-                        $numResults = $result->num_rows;
                         if($category == null) {
                             print("<h5 class=\"float-left\">$numResults resultaten voor '$search'</h5>");
                         } else {
                             print("<h5 class=\"float-left\">$numResults resultaten voor '$search' in category $categoryName</h5>");
                         }
                         ?>
-                        <select class="float-right custom-select w-25">
-                            <option value="" disabled="" selected>Sorteer op</option>
-                            <option value="name">Naam</option>
-                            <option value="dateNewOld">Datum: Nieuw-Oud</option>
-                            <option value="dateOldNew">Datum: Oud-Nieuw</option>
-                            <option value="priceLowHigh">Prijs: Laag-Hoog</option>
-                            <option value="priceHighLow">Prijs: Hoog-Laag</option>
+                        <script>
+                            function setParam(key, value) {
+                                //Check if current key already exists
+                                let url = new window.URL(window.location.href);
+                                let searchParams = url.searchParams;
+                                searchParams.set(key, value);
+                                url.searchParams = searchParams;
+                                return window.location = url;
+                            }
+                        </script>
+                        <select class="float-right custom-select w-25" onchange="setParam('order', this.value);">
+                            <option value="" disabled="" >Sorteer op</option>
+                            <option value="nameAZ" <?php if($_GET['order'] == "nameAZ") print("selected");?>>Naam: A-Z</option>
+                            <option value="nameZA" <?php if($_GET['order'] == "nameZA") print("selected");?>>Naam: Z-A</option>
+                            <option value="priceLowHigh" <?php if($_GET['order'] == "priceLowHigh") print("selected");?>>Prijs: Laag-Hoog</option>
+                            <option value="priceHighLow" <?php if($_GET['order'] == "priceHighLow") print("selected");?>>Prijs: Hoog-Laag</option>
                         </select>
                     </div>
-                    <ul class="list-group list-group-flush">
+                    <ul class="list-group list-group-flush" id="productList">
                         <li class="list-group-item">
                             <div>
 
                             </div>
                         </li>
                         <?php
+                        $resultArray = array();
                         while($row = $result->fetch_assoc()) {
-                            $productName = strip_tags($row['StockItemName']);
-                            $productPrice = strip_tags($row['RecommendedRetailPrice']);
-                            $productPhoto = base64_encode($row['Photo']);
+                            $resultArray[] = $row;
+                        }
+                        for($i = 0; $i < $numOfItemsPerPage; $i++) {
+                            $productName = strip_tags($resultArray[$i]['StockItemName']);
+                            $productPrice = strip_tags($resultArray[$i]['RecommendedRetailPrice']);
+                            $productPhoto = base64_encode($resultArray[$i]['Photo']);
                             print('<li class="list-group-item shadow"><img src="data:image/jpeg;base64,' . $productPhoto . '" width="100" height="100"><span class="col-8">'.$productName.'</span><span class="col-4">'. $productPrice.'</span></li>');
                         }
                         ?>
-
+                        <script>
+                            //now put it into the javascript
+                            let arrayObjects = [<?php
+                                    foreach ($resultArray as $item) {
+                                        $productName = $item['StockItemName'];
+                                        $productPrice = $item['RecommendedRetailPrice'];
+                                        print("{'StockItemName':`$productName`, 'RecommendedRetailPrice': `$productPrice`},");
+                                    }
+                                ?>];
+                            console.log(arrayObjects);
+                        </script>
                     </ul>
                 </div>
             </div>
