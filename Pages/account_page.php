458a54d58354c99db0a33cc8e9a5770dd3a06ddf
sanderpol <sander__pol@outlook.com>
2019-11-30 18:53:29 +0100

    not finished

--- Pages/account_page.php
+++ Pages/account_page.php
@@ -1,52 +1,80 @@
 <?php
 include "../BackgroundCode/SessionCode.php";
-include "../BackgroundCode/account_background.php";
-include "../DatabaseFactory.php";
 checkSessionActive();
-$userID = getUserID();
-$dbConnection = startDBConnection();
-$currentUser = getUser($userID, $dbConnection);
-
-
 include_once "../Modules/functions.php";
 print_header();
+include "../BackgroundCode/account_background.php";
+$userID = $_SESSION['userNr'];
+$currentUser = getUser($userID, $connection);
+$orders = getAssociatedOrders($userID, $connection)
+
 ?>
 <div class="container">
     <div class="col-sm-10 mt-5">
         <h1>Account</h1>
     </div>
     <div class="row">
-        <div class="col-md-6 mt-5 mb-5 ">
-            <p class="border p-3"><strong>Accountgegevens</strong><br>
-                AccountNr:<?php echo $currentUser[0]?><br>
-                Voledige Naam:<?php echo $currentUser[1]?><br>
-                Telefoon: <?php echo $currentUser[2]?><br>
-                E-Mail:<?php echo $currentUser[3]?><br>
-            </p>
-            <button class="mb-2 btn-primary pt-1 pb-1 pl-2 px-2 rounded"><strong>Verander accountgegevens</strong>
-            </button>
+        <div class="col-md-6 mt-5">
+            <div class="row m-1">
+                <div class="col-6 p-0 pl-2 border-top border-left border-bottom">
+                    <p class="pt-3"><strong>Accountgegevens</strong><br>
+                        AccountNr:<br>
+                        Voledige Naam:<br>
+                        Telefoon:<br>
+                        E-Mail:
+                    </p>
+                </div>
+                <div class="col-6 p-0 border-top border-right border-bottom">
+                    <p class="pt-3">
+                        <br>
+                        <?php echo $currentUser[0] ?><br>
+                        <?php echo $currentUser[1] ?><br>
+                        <?php echo $currentUser[2] ?><br>
+                        <?php echo $currentUser[3] ?>
+                    </p>
+                </div>
+                <button class="mb-2 btn-primary mt-5 pt-1 pb-1 pl-2 px-2 rounded"><strong>Verander
+                        accountgegevens</strong>
+                </button>
+            </div>
+            <div class="row m-2">
+                <div class="col-6 p-0 pl-2 border-top border-left border-bottom">
+                    <p class=" pt-3"><strong>Adresgegevens</strong><br>
+                        Straatnaam:<br>
+                        Postcode:<br>
+                        Plaatsnaam:<br>
+                        Provincie:<br>
+                        Land:<br>
+                    </p>
+                </div>
+                <div class="col-6 p-0 border-top border-right border-bottom">
+                    <p class=" pt-3">
+                        <br>
+                        <?php echo $currentUser[0] ?><br>
+                        <?php echo $currentUser[1] ?><br>
+                        <?php echo $currentUser[2] ?><br>
+                        <?php echo $currentUser[3] ?>
+                    </p>
+                </div>
+                <button class="mb-2 btn-primary mt-5 pt-1 pb-1 pl-2 px-2 rounded"><strong>Verander
+                        adresgegevens</strong>
+                </button>
+            </div>
         </div>
         <div class="col-md-6 mt-5 mb-5 ">
-            <p class="border p-3"><strong>Order geschiedenis</strong><br>
-                order: <a href="bestelgeschiedenispagina.php">4046</a>
-            </p>
+            <div class=" m-1 ">
+                <?php if ($orders != null) { ?>
+                    <p class="border col-12 pt-3"><strong>Order geschiedenis</strong><br>
+                        <?php foreach ($orders as $order) { ?>
+                            <a href="bestelgeschiedenispagina.php?orderId=<?php echo $order ?>">
+                                Order: <?php echo $order ?></a>
+                        <?php } ?>
+                    </p>
+                <?php } ?>
+            </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-6 mt-5 mb-5 ">
-            <p class="border p-3"><strong>Adresgegevens</strong><br>
-                Straatnaam:<br>
-                Postcode:<br>
-                Plaatsnaam:<br>
-                Provincie:<br>
-                Land:<br>
-            </p>
-            <button class="mb-2 btn-primary pt-1 pb-1 pl-2 px-2 rounded"><strong>Verander adresgegevens</strong>
-            </button>
-        </div>
-    </div>
-</div>
-<?php
-print_footer();
-?>
+    <?php
+    print_footer();
+    ?>
 
