b28dd7b132a6e372842237f94a7d11c626027df3
sanderpol <47556126+sanderpol@users.noreply.github.com>
2019-12-01 17:32:23 +0100

    fixing bug

--- BackgroundCode/Bestelgeschiedenis_functionali.php
+++ BackgroundCode/Bestelgeschiedenis_functionali.php
@@ -7,9 +7,9 @@ function getSqlResults($orderIDget, $userNr, $connection)
     $query = mysqli_prepare($connection, "CALL order_history(?,?)");
     $query->bind_param("ii", $orderIDget, $userNr);
     $query->execute();
-    $query->bind_result($contactPersonID, $orderID, $quantity, $stockItemName, $unitPrice);
+    $query->bind_result($contactPersonID, $orderID, $quantity, $stockItemName, $unitPrice, $photo);
     while ($query->fetch()) {
-        $row = array($contactPersonID, $orderID, $quantity, $stockItemName, $unitPrice);
+        $row = array($contactPersonID, $orderID, $quantity, $stockItemName, $unitPrice, $photo);
         $results[] = $row;
     }
     return $results;
--- BackgroundCode/SessionCode.php
+++ BackgroundCode/SessionCode.php
@@ -2,7 +2,7 @@
 
 function checkSessionActive()
 {
-    session_start();
+
     if (isset($_SESSION['loggedin']) == FALSE || $_SESSION['loggedin'] == FALSE) {
         header("Refresh: 0; url=login.php");
         exit();
