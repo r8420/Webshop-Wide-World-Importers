eb29aed053fc6b177d98f2c9b5de5aab6243387f
Vincent Houtjes <vincent@houtjes.com>
2019-12-09 13:38:24 +0100

    Winkelwagen - idk what, but I'm committing

--- Pages/winkelwagen.php
+++ Pages/winkelwagen.php
@@ -31,6 +31,8 @@ include "../BackgroundCode/winkelwagen_backcode.php";
                 $totalPrice = 0;
                 foreach ($shoppingCart as $productID=>$amount) {
                     $productInfo = getProductInformation($productID);
+                    $numberInStock = getProductStock($productID);
+
                     $productName = strip_tags($productInfo['StockItemName']);
                     $productPriceFormatted = strip_tags($productInfo['price']);
                     $productPrice = strip_tags($productInfo['UnitPrice']);
@@ -47,7 +49,7 @@ include "../BackgroundCode/winkelwagen_backcode.php";
                             <a href="product_pagina.php?product='.$productID.'">'.$productName.'</a>
                         </td>
                         <td>
-                            <input min="0" onchange="sendPostRequest('.$productID.', this.value, '.$productPrice.')" class="form-control w-40" type="number" value="'.$amount.'">
+                            <input min="0" max="'.$numberInStock.'" onchange="sendPostRequest('.$productID.', this.value, '.$productPrice.')" class="form-control w-60" type="number" value="'.$amount.'">
                         </td>
                         <td>€'.$productPriceFormatted.'</td>
                         <td id="productPrice'.$productID.'">€'.number_format($productPrice*$amount, 2, ',', '.').'</td>
